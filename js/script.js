64c2e4fd8379a22aec1b5865c43818406fec02eb
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-12 17:06:24 +0200

    Inizializzato progetto

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+const numberKilometers = prompt ('inserisci il numeno di kilometri');
+const Age = prompt ("Inserisci l'età del passeggero ");
+
+// ------------------
+
+const verificationKilo = parseInt(numberKilometers)
+const verificationAge = parseInt(Age)
+
+
+
+
+
+
+
+
+// VALIDAZIONE 
+if (isNaN (verificationAge)) {
+
+    alert('Non è un numero')
+    
+    }
+
+if (isNaN (verificationKilo)) {
+
+    alert('Non è un numero')
+    
+    }
